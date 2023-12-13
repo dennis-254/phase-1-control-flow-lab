24504c78d885aaa2d726b093d936aededc897e2c
+dennis.mutua@student.moringaschool.com <dennis@DESKTOP-Q9A0DBN.localdomain>
2023-12-13 22:35:51 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    return "No can do.";
+  }
+}
+function ternaryCheckCity(city) {
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else if (city !== "NYC") {
+    return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let message;
+  switch (tip) {
+    case "generous":
+      message = "Thank you so much.";
+      break;
+    case "not as generous":
+      message = "Thank you.";
+      break;
+    case "thanks for everything":
+      message = "Bye.";
+      break;
+  }
+  return message
 }
